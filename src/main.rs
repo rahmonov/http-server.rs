4b291ad1aa48c20afb4b15d4f304b59451581184
Jakhongir Rakhmonov <jrahmonov2@gmail.com>
2025-08-07 10:45:31 +0200

    Badly parse the request path and return 404 if not home page

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::{io::Write, net::TcpListener};
+use anyhow::Result;
+use std::{
+    io::{BufRead, BufReader, BufWriter, Write},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
+fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -8,13 +12,43 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                stream.write_all(response.as_bytes()).unwrap();
+            Ok(stream) => {
+                handle_connection(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
+}
+
+fn handle_connection(stream: TcpStream) -> Result<()> {
+    println!("Handling a connection...");
+
+    let mut buffer = String::new();
+    let read_stream = stream.try_clone()?;
+    let write_stream = stream;
+
+    let mut reader = BufReader::new(read_stream);
+    let mut writer = BufWriter::new(write_stream);
+
+    let good_response = "HTTP/1.1 200 OK\r\n\r\n";
+    let not_found_resp = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    reader.read_line(&mut buffer)?;
+
+    let parts = buffer.split_whitespace().collect::<Vec<&str>>();
+    let request_path = parts[1];
+
+    if request_path == "/" {
+        writer.write_all(good_response.as_bytes())?;
+    } else {
+        writer.write_all(not_found_resp.as_bytes())?;
+    }
+
+    println!("received request {parts:?}");
+
+    Ok(())
 }
